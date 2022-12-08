e779831d5c3767bcfd77e993285fc60a926e841f
Julietndlamini <jndlamini3@gmail.com>
2022-12-08 11:08:07 -0800

    my first line of code c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingu        al puzzle");
+	puts("\|"Programming is like building a multiling        ual puzzle");
 	return(0);
 } 
