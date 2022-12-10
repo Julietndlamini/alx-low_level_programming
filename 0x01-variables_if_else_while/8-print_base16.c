dbbc888352786910e896d07389e6b7a9ccd587af
Julietndlamini <jndlamini3@gmail.com>
2022-12-09 23:48:01 -0800

    task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,16 +1,24 @@
 #include <stdio.h>
 
 /**
- * main - Prints the lowercase alphabet in reverse.
+ * main - Prints all possible combination of dingle digit numbers.
  *
  * Return: Always 0.
 */
 int main(void)
 {
-	char letter;
+	int num;
 
-	for (letter = 'z'; letter >= 'a'; letter--)
-		putchar(letter);
+	for (num = 0; num <= 9; num++)
+	{
+		putchar((num % 10) + '0');
+		if (num == 9)
+			continue;
+
+		putchar(',');
+		putchar(',');
+	}
+
 
 putchar('\n');
 
