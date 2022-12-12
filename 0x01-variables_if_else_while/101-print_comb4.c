3634c201000d7dc1bbc3c7417169d8b5c55086cb
Julietndlamini <jndlamini3@gmail.com>
2022-12-12 06:31:33 -0800

    advanced 2

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,42 +1,43 @@
 #include <stdio.h>
+
 /**
- * main - Prints all possible different combination of three didigts.
- *
- * Return: Always 0
- */
+* main - Print all possible combinations of three
+* different digits, in ascending order, separated
+* by comma and space.
+*
+* Return: Always 0
+*/
 int main(void)
 {
-	int ones = '0';
-	int tens = '0';
-	int hundreds = '0';
+	int digit1, digit2, digiti3;
 
-	for (hundreds = '0'; hundreds <= '9'; hundreds++)
+	for (digit1 = 0; digit1 < 8; digit1++)
 	{
-		for (tens = '0'; tens <= '9'; tens++)
+		for (digit2 = digit1 + 1; digit2 < 9;
+				digit2++)
 		{
-			for (ones = '0'; ones <= '9'; ones++)
+			for (digit3 = digit2 + 1;
+			digit3 < 10; digit3++)
 			{
-				if (!((ones == tens) ||
-				(tens == hundreds) ||
-				(tens > ones) ||
-				(hundreds > tens)))
-				/*eliminates repetition*/				  {
-				  putchar(hundreds);
-				  putchar(tens);
-				  putchar(ones);
-				  if (!((ones is == '9';
-				  && hundreds == '7';
-				  && tens == '8')))*|
-				  adds commas and space*
-				  /
-				  {
-				  putchar(',')
-				  putchar(' ')
-				  }
-				  }
-				  }
-				  }
-				  }
-				  putchar('\n')
-				  return (0);
-}
+				putchar((digit1 % 10)
+						+ '0');
+				putchar((digit2 % 10)
+						+ '0');
+				putchar((digit3 % 10)
+						+ '0');
+				{
+				if (digit1 == 7 &&
+				digit2 == 8
+				digit3 == 9;)
+					continue;
+
+				putchar(',');
+				putchar(' ');
+				}
+			}
+		}
+
+		putchar('\n');
+
+		return (0);
+	}
