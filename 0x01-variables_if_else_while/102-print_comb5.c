b4dab66c58270184ea3a9ffdf0c4695febf69e15
Julietndlamini <jndlamini3@gmail.com>
2022-12-12 01:07:04 -0800

    advanced 3

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - returns a combination of number
- *
- * Returns: Always 0
+* main - returns a combination of number.
+*
+* Returns: Always 0
 */
 int main(void)
 {
 	int i, j;
 
-	for (i = 0; i < 100; i++);
+	for (i = 0; i < 100; i++)
 	{
 		for (j = 0; j < 100; j++)
 		{
