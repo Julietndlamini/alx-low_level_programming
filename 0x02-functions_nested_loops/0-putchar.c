8d2bd0e6ca590d85e9bcebe13cae6ed1e68b1c86
Julietndlamini <jndlamini3@gmail.com>
2022-12-13 09:37:34 -0800

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 /**
- * main - Print _putchar
- * is written by j ndlamini
- * Return: Always 0
- */
+*main - Print _putchar
+*is written by j ndlamini
+* Return: Always 0
+*/
+
 int main(void)
 {
 	_putchar('_');
@@ -15,6 +16,6 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
-	_putchar('\n');
+	-putchar('\n');
 	return (0);
 }
