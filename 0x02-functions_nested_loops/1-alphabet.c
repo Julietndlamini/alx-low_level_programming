b365a2f38588ed32414ff212ce099e530734a57a
Julietndlamini <jndlamini3@gmail.com>
2022-12-13 08:16:31 -0800

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercase,
+ * followed by a new line.
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'b'; letter <= 'd'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
+}
+
