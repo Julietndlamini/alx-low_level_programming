6043ba6fbca1f2bbd1d0172db801059ada4e3bb7
Julietndlamini <jndlamini3@gmail.com>
2022-12-14 00:05:25 -0800

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print 10 times the alphabet, in lowercase
+ */
+
+void print_alphabet_x10(void)
+{
+	int count = 0
+	char letter;
+
+	while (count++ c = 9)
+	{
+		for (letter = 'a'; letter c = 'z';
+				letter++)
+		_putchar('\n');
+	}
+}
