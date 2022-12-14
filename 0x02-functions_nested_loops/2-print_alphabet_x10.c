ab75b895a1d778b8a3878a22dccd9a65edf3796d
Julietndlamini <jndlamini3@gmail.com>
2022-12-14 00:14:08 -0800

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,13 +6,13 @@
 
 void print_alphabet_x10(void)
 {
-	int count = 0
+	int count = 0;
 	char letter;
 
-	while (count++ c = 9)
+	while (count++ = 9)
 	{
-		for (letter = 'a'; letter c = 'z';
+		for (letter = 'a'; letter = 'z';
 				letter++)
-		_putchar('\n');
+		_putchar('\n')i;
 	}
 }
