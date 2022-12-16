8831d6909318e915446c86837ea331f7b9dfc403
Julietndlamini <jndlamini3@gmail.com>
2022-12-15 19:34:09 -0800

    advanced 2

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,19 +7,16 @@
 
 void print_number(int n)
 {
-unsigned int n1;
+unsigned int i = n;
+
 if (n < 0)
 {
-n1 = -n;
-_putchar('-');
+_putchar(45);
+i = -i;
 }
-else
+if (i / 10)
 {
-n1 = n;
+print_number(i / 10);
 }
-if (n1 / 10)
-{
-print_number(n1 / 10);
-}
-_putchar((n1 % 10) + '0');
+_putchar(i % 10 + '0');
 }
