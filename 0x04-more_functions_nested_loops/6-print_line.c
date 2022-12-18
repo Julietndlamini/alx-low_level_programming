398df4f7b09357f0797a68955c5d26ea9308096f
Julietndlamini <jndlamini3@gmail.com>
2022-12-18 12:24:04 -0800

    task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,23 +1,25 @@
 #include "main.h"
 
 /**
- * more_numbers - prints 10 times the numbers
- *
- * Return: 0
- */
-void more_numbers(void)
+* print_line - draws a straight line in the terminal
+* @n: number of times the character _ should be printed
+*/
+void print_line(int n)
 {
-	int x;
-	int y;
-
-	for (x = 0; x < 10; x++)
+	if (n <= 0)
 	{
-		for (y = 0; y <= 14; y++)
+		_putchar('\n');
+	}
+	else
+	{
+		int i;
+
+		for (i = 1; i <= n; i++)
 		{
-			if (y >= 10)
-				_putchar((y / 10) + '0');
-			_putchar((y % 10) + '0');
+		_putchar('_');
 		}
 		_putchar('\n');
 	}
+
 }
+
