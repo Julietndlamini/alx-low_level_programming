e6df703ac51b9ec127d82e8a02ffc53088407ad3
Julietndlamini <jndlamini3@gmail.com>
2022-12-17 22:45:51 -0800

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
-* print_most_numbers - prints
-*owned by juls
-* Return: nothing
-*/
+ * print_most_numbers - prints numbers
+ *
+ * Return: nothing
+ */
 void print_most_numbers(void)
 {
 	int i;
 
 	for (i = 0; i <= 9; i++)
 	{
-		if (i != 2) || (i != 4)
-		_putchar(i + '0');
+		if (i != 2 && i != 4)
+			_putchar(i + '0');
 	}
 	_putchar('\n');
 }
